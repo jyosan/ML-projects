ded73d556c55a4d266dec57965d622b937c59166
jyosan <jyo_mails@yahoo.com>
2018-04-29 07:00:02 -0400

    commited
    new file:   ML-ex1/ex1/warmUpExercise.m
    commited on 04/29 Jyo Krish added coursera ML ex1
    [M *L
    
    :wq

---
+++ ML-ex1/ex1/gradientDescentMulti.m
@@ -0,0 +1,58 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+%X = [ones(m, 1) X];
+
+fprintf('aaaaaaaaaa')
+size(X)
+size(theta)
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+
+
+	 	
+% thetanew = theta ;	
+
+%for j= 1:p
+ 
+  
+%thetanew(j) = theta(j) - (alpha/m*sum(((X*theta) -y)*X(:,i)));
+%end;
+%theta = thetanew;
+
+%delta = ((theta' * X' - y')*X)';
+  %theta = theta - alpha / m * delta;
+
+ %theta = theta - (alpha/m) * X'*(X*theta -y );
+ 
+ %temp = theta - (alpha/m) * (((X * theta) - y)' * X)';
+%theta = temp;
+
+u=X*theta;
+
+%theta = theta - (alpha/length(y))*X' * (X*theta - y);
+
+
+
+
+% ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCostMulti(X, y, theta);
+
+end
+
+end
